96ab7f5e5ac9cc7f987e65d42a256b8a6cfcd81c
Anatolii Mishchenko <87mischenko@mail.ru>
2022-08-12 18:12:46 +0700

    Final Home Work Sem Free

--- Sem_3/Task_1 home work/Program.cs
+++ Sem_3/Task_1 home work/Program.cs
@@ -1,12 +1,23 @@
 ﻿
 Console.WriteLine("Введите чило из пяти числе");
 
- string array = Console.ReadLine();
-if(array[0] == array[4] && array[1]==array[3]){
-    Console.WriteLine("Чило введенное вами является полидромом");
-}
-else{
-    Console.WriteLine("Чило введенное вами не является полидромом");
+string array = Console.ReadLine();
+Console.WriteLine(array.Length);
 
+if (array.Length == 5)
+{
+    if (array[0] == array[4] && array[1] == array[3])
+    {
+        Console.WriteLine("Чило введенное вами число является полидромом");
+    }
+    else
+    {
+        Console.WriteLine("Чило введенное вами число не является полидромом");
+
+    }
+}
+else
+{
+    Console.WriteLine("Введенное вами число не пятизначное");
 }
 
